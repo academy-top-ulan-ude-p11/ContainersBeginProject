7de66031aa0b99ba59990fedeb6038f73b65c438
GitStudent2022 <torelgame@mail.ru>
2022-10-25 14:54:11 +0300

    Добавьте файлы проекта.

---
+++ ContainersBeginProject/ContainersBeginProject.cpp
@@ -0,0 +1,28 @@
+﻿// ContainersBeginProject.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include <iostream>
+#include <vector>
+
+class User
+{
+    char name[50];
+    int age;
+};
+
+using namespace std;
+
+int main()
+{
+
+    vector<int> v;
+    for (int i = 0; i < 15; i++)
+    {
+        cout << i << ": capacity: " << v.capacity() << " size: " << v.size() << "\n";
+        v.push_back(i + 1);
+    }
+        
+    for (int i = 0; i < 10; i++)
+        cout << v[i] << " ";
+
+    vector<User> users;
+}
